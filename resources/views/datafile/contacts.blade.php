cd167365e1d1dbb60359c59f945408d037efaeec
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-01-10 18:51:19 +0500

    ok

--- resources/views/datafile/contacts.blade.php
+++ resources/views/datafile/contacts.blade.php
@@ -13,31 +13,22 @@
                     <table id="myTable" class="table table-sm table-bordered table-hover display  margin-top-10 w-p100">
                         <thead>
                         <tr>
-                            <th>Device</th>
-                            <th>Files Count</th>
-                            <th>Last Update</th>
-                            <th>Actions</th>
+                            <th>Name</th>
+                            <th>Phone</th>
+
                         </tr>
                         </thead>
                         @if($item->contacts)
                         @foreach($item->contacts as $item2)
                         <tr>
-                            <td>Vivo Y20</td>
-                            <td>200</td>
-                            <td>01-01-2023</td>
                             <td>
-                                <div class="pull-right">
-                                    <button type="button" class="btn btn-light btn-round btn-page-header-dropdown dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fa fa-ellipsis-h"></i>
-                                    </button>
-                                    <div class="dropdown-menu">
-                                        <div class="arrow"></div>
-                                        <a class="dropdown-item" href="#">Fetch New Data</a>
-                                        <a class="dropdown-item" href="#">View All Device Data</a>
-
-                                    </div>
-                                </div>
+                                @if($item2['_objectInstance'])
+                                    {{$item2['_objectInstance']}}
+                                    @endif
                             </td>
+                            <td>200</td>
+
+
                         </tr>
                         @endforeach
                         @endif
