61545435a81fbfbc81e24bb587bee7145b02c9b6
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-01-10 19:12:21 +0500

    ok

--- app/Http/Controllers/DatafileController.php
+++ app/Http/Controllers/DatafileController.php
@@ -25,13 +25,10 @@ class DatafileController extends Controller
 
       if(is_countable(json_decode($it->contacts)))
       {
-          $c=$it->contacts=json_decode($it->contacts);
-          if (isset($c['_objectInstance']))
-          {
-              dd($c['_objectInstance']);
-          }
+          $it->contacts=json_decode($it->contacts);
+
           //asdas
-dd($c);
+dd($it->contacts[0]['_objectInstance']);
       }else
       {
           $it->contacts=[];
