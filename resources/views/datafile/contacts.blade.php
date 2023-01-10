8ebad8938ae49bd1993f53e95667a862072e5824
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-01-10 19:27:15 +0500

    ok

--- resources/views/datafile/contacts.blade.php
+++ resources/views/datafile/contacts.blade.php
@@ -27,7 +27,7 @@
                                     $name=$item2->_objectInstance->name;
 
                                   $nameArray=(array)$name;
-                                  dd($nameArray['formatted']);
+                                  echo strval($nameArray['formatted']);
                                     ?>
 
 
